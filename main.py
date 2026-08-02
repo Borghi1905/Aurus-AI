b346a1b130aff8adcbecd782578573bd5bb3d8ce
Gabriel Borghi <gborghiwork@gmail.com>
2026-08-02 15:29:21 -0300

    adicionando a primeira parte individual

--- main.py
+++ main.py
@@ -127,3 +127,189 @@ media_omeprazol = df_pacientes["dose_omeprazol"].mean()
 media_metformina = df_pacientes["dose_metformina"].mean()
 media_creatina = df_pacientes["dose_creatina"].mean()
 
+# parte de individualização, para pegar as informações de cada paciente
+
+id_desejado = int(input("Digite o ID do paciente desejado: "))
+
+# escolhendo o paciente
+paciente = df_pacientes[df_pacientes["paciente_id"] == id_desejado]
+
+# filtro se o paciente existe ou não, caso não exista, o programa é encerrado
+if paciente.empty:
+    print("Paciente não encontrado.")
+    exit()
+
+# transforma o DataFrame em uma Series (uma única linha)
+paciente = paciente.iloc[0]
+
+# pegando as informações do paciente
+idade = paciente["idade"]
+peso = paciente["peso_kg"]
+metabolismo = paciente["metabolismo"]
+sensibilidade_farmacologica = paciente["sensibilidade_farmacologica"]
+indice_saude_base = paciente["indice_saude_base"]
+
+dose_cafeina = paciente["dose_cafeina"]
+dose_alcool = paciente["dose_alcool"]
+dose_nicotina = paciente["dose_nicotina"]
+dose_melatonina = paciente["dose_melatonina"]
+dose_ibuprofeno = paciente["dose_ibuprofeno"]
+dose_dipirona = paciente["dose_dipirona"]
+dose_loratadina = paciente["dose_loratadina"]
+dose_omeprazol = paciente["dose_omeprazol"]
+dose_metformina = paciente["dose_metformina"]
+dose_creatina = paciente["dose_creatina"]
+
+frequencia_cardiaca_bpm = paciente["frequencia_cardiaca_bpm"]
+nivel_sonolencia = paciente["nivel_sonolencia"]
+nivel_ansiedade = paciente["nivel_ansiedade"]
+
+
+
+# Comparações do paciente com a média do dataset
+
+# Frequência Cardíaca
+if frequencia_cardiaca_bpm > frequencia_cardiaca_media:
+    comparacao_frequencia_cardiaca = "Acima da média"
+elif frequencia_cardiaca_bpm < frequencia_cardiaca_media:
+    comparacao_frequencia_cardiaca = "Abaixo da média"
+else:
+    comparacao_frequencia_cardiaca = "Na média"
+
+# Índice de Saúde
+if indice_saude_base > indice_de_saude_medio:
+    comparacao_indice_saude = "Acima da média"
+elif indice_saude_base < indice_de_saude_medio:
+    comparacao_indice_saude = "Abaixo da média"
+else:
+    comparacao_indice_saude = "Na média"
+
+# Ansiedade
+if nivel_ansiedade > nivel_de_ansiedade_medio:
+    comparacao_nivel_ansiedade = "Acima da média"
+elif nivel_ansiedade < nivel_de_ansiedade_medio:
+    comparacao_nivel_ansiedade = "Abaixo da média"
+else:
+    comparacao_nivel_ansiedade = "Na média"
+
+# Sonolência
+if nivel_sonolencia > nivel_de_sonolencia_medio:
+    comparacao_nivel_sonolencia = "Acima da média"
+elif nivel_sonolencia < nivel_de_sonolencia_medio:
+    comparacao_nivel_sonolencia = "Abaixo da média"
+else:
+    comparacao_nivel_sonolencia = "Na média"
+
+# Peso
+peso_medio = df_pacientes["peso_kg"].mean()
+
+if peso > peso_medio:
+    comparacao_peso = "Acima da média"
+elif peso < peso_medio:
+    comparacao_peso = "Abaixo da média"
+else:
+    comparacao_peso = "Na média"
+
+
+# Situação das doses em relação à média do dataset
+
+# Cafeína
+if dose_cafeina == 0:
+    status_cafeina = "Não utiliza"
+elif dose_cafeina > media_cafeina:
+    status_cafeina = "Acima da média"
+elif dose_cafeina < media_cafeina:
+    status_cafeina = "Abaixo da média"
+else:
+    status_cafeina = "Na média"
+
+# Álcool
+if dose_alcool == 0:
+    status_alcool = "Não utiliza"
+elif dose_alcool > media_alcool:
+    status_alcool = "Acima da média"
+elif dose_alcool < media_alcool:
+    status_alcool = "Abaixo da média"
+else:
+    status_alcool = "Na média"
+
+# Nicotina
+if dose_nicotina == 0:
+    status_nicotina = "Não utiliza"
+elif dose_nicotina > media_nicotina:
+    status_nicotina = "Acima da média"
+elif dose_nicotina < media_nicotina:
+    status_nicotina = "Abaixo da média"
+else:
+    status_nicotina = "Na média"
+
+# Melatonina
+if dose_melatonina == 0:
+    status_melatonina = "Não utiliza"
+elif dose_melatonina > media_melatonina:
+    status_melatonina = "Acima da média"
+elif dose_melatonina < media_melatonina:
+    status_melatonina = "Abaixo da média"
+else:
+    status_melatonina = "Na média"
+
+# Ibuprofeno
+if dose_ibuprofeno == 0:
+    status_ibuprofeno = "Não utiliza"
+elif dose_ibuprofeno > media_ibuprofeno:
+    status_ibuprofeno = "Acima da média"
+elif dose_ibuprofeno < media_ibuprofeno:
+    status_ibuprofeno = "Abaixo da média"
+else:
+    status_ibuprofeno = "Na média"
+
+# Dipirona
+if dose_dipirona == 0:
+    status_dipirona = "Não utiliza"
+elif dose_dipirona > media_dipirona:
+    status_dipirona = "Acima da média"
+elif dose_dipirona < media_dipirona:
+    status_dipirona = "Abaixo da média"
+else:
+    status_dipirona = "Na média"
+
+# Loratadina
+if dose_loratadina == 0:
+    status_loratadina = "Não utiliza"
+elif dose_loratadina > media_loratadina:
+    status_loratadina = "Acima da média"
+elif dose_loratadina < media_loratadina:
+    status_loratadina = "Abaixo da média"
+else:
+    status_loratadina = "Na média"
+
+# Omeprazol
+if dose_omeprazol == 0:
+    status_omeprazol = "Não utiliza"
+elif dose_omeprazol > media_omeprazol:
+    status_omeprazol = "Acima da média"
+elif dose_omeprazol < media_omeprazol:
+    status_omeprazol = "Abaixo da média"
+else:
+    status_omeprazol = "Na média"
+
+# Metformina
+if dose_metformina == 0:
+    status_metformina = "Não utiliza"
+elif dose_metformina > media_metformina:
+    status_metformina = "Acima da média"
+elif dose_metformina < media_metformina:
+    status_metformina = "Abaixo da média"
+else:
+    status_metformina = "Na média"
+
+# Creatina
+if dose_creatina == 0:
+    status_creatina = "Não utiliza"
+elif dose_creatina > media_creatina:
+    status_creatina = "Acima da média"
+elif dose_creatina < media_creatina:
+    status_creatina = "Abaixo da média"
+else:
+    status_creatina = "Na média"
+
